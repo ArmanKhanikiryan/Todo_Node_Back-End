37bbc81aef98215ada4ee1e3fedff990bb1bb392
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-01-08 01:53:27 +0400

    mongoose ready"

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
 import cors from "cors"
 import {MongoClient, ObjectId} from 'mongodb'
-
+import mongoose, {Schema} from "mongoose";
 const client = new MongoClient('mongodb://localhost:27017');
-
-
 (async function () {
     await client.connect();
+
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb://localhost:27017', () => {
+    });
+
+    mongoose.set('strictQuery', true)
+
+    let TodoSchema = new Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        isChecked: {
+            type: Boolean,
+            required: true
+        }
+    }, {timestamps: true})
+
+    const TodoModel = mongoose.model("Todo", TodoSchema);
 
     const cleanup = (event) => {
         client.close();
@@ -15,9 +32,12 @@ const client = new MongoClient('mongodb://localhost:27017');
     process.on('SIGINT', cleanup);
     process.on('SIGTERM', cleanup);
 
-    const dataBase = client.db('node_project')
-    const collection = dataBase.collection('todos')
+
+    // const dataBase = TodoModel.db
+    // const collection = TodoModel.collection
+    // collection.collectionName = 'Mongoose_Todo'
     const app = express()
+
 
     app.use(express.static('public'))
     app.use(express.json())
@@ -25,38 +45,59 @@ const client = new MongoClient('mongodb://localhost:27017');
 
 
     app.get('/todos', async (req, res) => {
-        const allTodos = await collection.find({}).toArray()
-        res.send(allTodos)
+        const allTodos = await TodoModel.find()
+        res.status(200).json(allTodos)
     })
 
     app.post('/todos', async (req, res) => {
 
-        await collection.insertOne({title: req.body.title, isChecked: false})
-        const allTodos = await collection.find({}).toArray()
+        const newTodo = new TodoModel({
+            title: req.body.title,
+            isChecked: false
+        })
+        await newTodo.save()
+        const allTodos = await TodoModel.find()
         res.send(allTodos)
+
+        // await collection.insertOne({title: req.body.title, isChecked: false})
+        // const allTodos = await collection.find({}).toArray()
+        // console.log(allTodos, 'ALL')
+        // res.send(allTodos)
     })
 
     app.delete('/todos',  async (req, res) => {
 
-        const query = {_id: new ObjectId(req.body._id)}
-        await collection.deleteOne(query)
-        const allTodos = await collection.find({}).toArray()
+
+        await TodoModel.findByIdAndDelete(req.body._id)
+        const allTodos = await TodoModel.find()
         res.send(allTodos)
+
+        // const query = {_id: new ObjectId(req.body._id)}
+        // await collection.deleteOne(query)
+        // const allTodos = await collection.find({}).toArray()
+        // res.send(allTodos)
     })
 
 
     app.put('/todos', async (req, res) => {
-        const query = {_id: new ObjectId(req.body._id)}
-        const todo = await collection.findOne(query)
+
+        const todo = await TodoModel.findById(req.body._id)
         const update = {
-            $set: {
-                "isChecked": !todo.isChecked
-            }
+            isChecked: !todo.isChecked
         }
-        await collection.updateOne(query, update)
-        const allTodos = await collection.find({}).toArray()
+        await TodoModel.findByIdAndUpdate(req.body._id, update)
+        const allTodos = await TodoModel.find()
         res.send(allTodos)
-        console.log(allTodos)
+        // const query = {_id: new ObjectId(req.body._id)}
+        // const todo = await collection.findOne(query)
+        // const update = {
+        //     $set: {
+        //         "isChecked": !todo.isChecked
+        //     }
+        // }
+        // await collection.updateOne(query, update)
+        // const allTodos = await collection.find({}).toArray()
+        // res.send(allTodos)
     })
     app.listen(3010)
 
